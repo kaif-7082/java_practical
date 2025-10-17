4d16b589938d75170318cd2ad3f275923aeea95b
KAIF-285 <kaif70882@gmail.com>
2025-10-17 14:53:41 +0530

    new update

--- carrental.java
+++ carrental.java
@@ -1,8 +1,9 @@
+import java.util.Optional;
 
 public class carrental {
     public static void main(String[] args) {
-        Car c1 = new Car("Tesla", "Model 3", 2023);
-        Car c2 = new Car("Toyota", "Innova");
+        Car c1 = new ElectricCar("Tesla", "Model 3", 2023, 400);
+        Car c2 = new Car("Toyota", null, 2025);
 
         c1.displayDetails();
         c2.displayDetails();
@@ -13,11 +14,9 @@ public class carrental {
         r1.displayBill();
         r2.displayBill();
 
-        
         System.out.println("Total Rentals Made: " + Rental.totalRentals);
     }
 }
-
 
 class Vehicle {
     String type;
@@ -31,57 +30,64 @@ class Vehicle {
     }
 }
 
-
 class Car extends Vehicle {
-    final String brand;   
-    String model;
+    final String brand;
+    Optional<String> model;
     int year;
 
     Car() {
-        super(); 
+        super();
         this.brand = "Unknown";
-        this.model = "Unknown";
+        this.model = Optional.empty();
         this.year = 0;
     }
 
-    
     Car(String brand, String model, int year) {
-        super();  
+        super();
         this.brand = brand;
-        this.model = model;
+        this.model = Optional.ofNullable(model);
         this.year = year;
     }
 
-    // Overloaded constructor using this()
-    Car(String brand, String model) {
-        this(brand, model, 2020);
-    }
-
     void displayDetails() {
-        super.showType();  // calling parent class method using super
-        System.out.println("Car: " + brand + " " + model + " (" + year + ")");
+        super.showType();
+        System.out.println("Car: " + brand + " " + model.orElse("Unknown Model") + " (" + year + ")");
         System.out.println("------------------------");
     }
 }
 
+class ElectricCar extends Car {
+    int batteryRange;
+
+    ElectricCar(String brand, String model, int year, int batteryRange) {
+        super(brand, model, year);
+        this.batteryRange = batteryRange;
+    }
+
+    @Override
+    void displayDetails() {
+        super.showType();
+        System.out.println("Electric Car: " + brand + " " + model.orElse("Unknown Model") +
+                " (" + year + "), Range: " + batteryRange + " km");
+        System.out.println("------------------------");
+    }
+}
 
 class Rental {
-    final double PRICE_PER_DAY = 1000.0; 
-    static int totalRentals = 0;        
+    final double PRICE_PER_DAY = 1000.0;
+    static int totalRentals = 0;
     Car car;
     int days;
 
-    
     Rental(Car car, int days) {
         this.car = car;
         this.days = days;
-        totalRentals++; 
+        totalRentals++;
     }
 
     void displayBill() {
         double total = days * PRICE_PER_DAY;
 
-       
         if (days > 3) {
             System.out.println("You got a discount for renting more than 3 days!");
             total = total * 0.9;
@@ -89,8 +95,8 @@ class Rental {
             System.out.println("No discount applied.");
         }
 
-        System.out.println("Rented " + car.model + " for " + days + " days. Total = ₹" + total);
+        System.out.println("Rented " + car.model.orElse("Unknown Model") +
+                " for " + days + " days. Total = ₹" + total);
         System.out.println("------------------------");
     }
 }
-
